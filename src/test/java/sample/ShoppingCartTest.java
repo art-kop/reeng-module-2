01cf01ddd2997076c02daed6929a02253d20dcce
artem koplyk <artem.koplyk@student.sumdu.edu.ua>
2021-12-30 11:22:54 +0200

    Initial commit

---
+++ src/test/java/sample/ShoppingCartTest.java
@@ -0,0 +1,38 @@
+package sample;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void formatTicket() {
+    }
+
+    @Test
+    void appendFormatted() {
+    }
+
+    @Test
+    void calculateDiscount() {
+    }
+}
