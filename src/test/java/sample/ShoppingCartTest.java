d088f3c45988b713d0159b8d8c6c0582b2cd7fd2
artem koplyk <artem.koplyk@student.sumdu.edu.ua>
2021-12-30 11:44:26 +0200

    Added tests

--- src/test/java/sample/ShoppingCartTest.java
+++ src/test/java/sample/ShoppingCartTest.java
@@ -1,20 +1,10 @@
 package sample;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
 
     @Test
     void main() {
@@ -30,9 +20,49 @@ class ShoppingCartTest {
 
     @Test
     void appendFormatted() {
+        StringBuilder sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Word", 0, 3);
+        assertEquals("Wor ", sb.toString());
+
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Word", 0, 10);
+        assertEquals("   Word    ", sb.toString());
+
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Word", 0, 15);
+        assertEquals("     Word       ", sb.toString());
+
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Word", 1, 15);
+        assertEquals("           Word ", sb.toString());
+
+        sb = new StringBuilder();
+        ShoppingCart.appendFormatted(sb, "Word", -1, 15);
+        assertEquals("Word            ", sb.toString());
     }
 
     @Test
     void calculateDiscount() {
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 1));
+        assertEquals(10, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 101));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 800));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.REGULAR, 1000));
+
+        assertEquals(70, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 1));
+        assertEquals(70, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 9));
+        assertEquals(71, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 10));
+        assertEquals(73, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 30));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SALE, 500));
+
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 1));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 10));
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.NEW, 20));
+
+        assertEquals(0, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 1));
+        assertEquals(50, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 2));
+        assertEquals(50, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 9));
+        assertEquals(51, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 10));
+        assertEquals(53, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 30));
+        assertEquals(80, ShoppingCart.calculateDiscount(ShoppingCart.ItemType.SECOND_FREE, 500));
     }
 }
